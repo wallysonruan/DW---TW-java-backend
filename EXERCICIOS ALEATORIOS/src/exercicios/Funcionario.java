bffe9818ef91362ca6d8dc4d80e976fe2ade3cd3
wallyson ruan <wallysonruan@gmail.com>
2022-05-15 13:49:37 -0300

    reorganização do código da classe FUNCIONARIO

--- EXERCICIOS ALEATORIOS/src/exercicios/Funcionario.java
+++ EXERCICIOS ALEATORIOS/src/exercicios/Funcionario.java
@@ -5,23 +5,30 @@ public class Funcionario {
 	private String cargo;
 	private int salario;
 	
-	
+	//SETTLERS
 	public void defDados(String nome, String cargo, int salario) {
 		this.nome = nome;
 		this.cargo = cargo;
 		this.salario = salario;
 	}
 	
-	public void mostrarDados() {
-		System.out.printf("Nome: %s\nCargo: %s\nSalário: %d",this.nome, this.cargo, this.salario);
+	public void mudarNome(String novoNome){
+		this.nome = novoNome;
+	}
+	
+	public void mudarCargo (String novoCargo){
+		this.cargo = novoCargo;
 	}
 	
 	public void mudarSalario(int porcentagem){
 		this.salario = this.salario + ((this.salario / 100) * porcentagem);
 	}
-	
+	//GETTERS
 	public void mostrarSalario() {
 		System.out.print(this.salario);
 	}
 	
+	public void mostrarDados() {
+		System.out.printf("Nome: %s\nCargo: %s\nSalário: %d",this.nome, this.cargo, this.salario);
+	}
 }

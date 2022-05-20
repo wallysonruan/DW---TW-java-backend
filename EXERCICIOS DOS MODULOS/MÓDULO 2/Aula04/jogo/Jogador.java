56b48e028888f77814205dc3fddee69c1e714db6
wallyson ruan <wallysonruan@gmail.com>
2022-05-20 14:51:41 -0300

    Apaguei a Fuzil e M4a1, adiciona-las-ei quando finalizar as classes pai

--- EXERCICIOS DOS MODULOS/MÓDULO 2/Aula04/jogo/Jogador.java
+++ EXERCICIOS DOS MODULOS/MÓDULO 2/Aula04/jogo/Jogador.java
@@ -1,8 +1,6 @@
 package jogo;
 
 public class Jogador {
-
-	M4a1 m = new M4a1();
 	
 	public static void main(String[] args) {
 		
--- EXERCICIOS DOS MODULOS/MÓDULO 2/Aula04/jogo/M4a1.java
+++
@@ -1,9 +0,0 @@
-package jogo;
-
-public class M4a1 extends Arma {
-
-	public M4a1() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
